9663f2bcf0082b6b873b7e1f8d39fd9ec1ac07ea
Titas212 <titas2001@gmail.com>
2022-11-30 15:04:02 +0200

    Changed credentials

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 mvn package shade:shade
 
-APP_ID=a3ff3e03 \
-API_KEY=9b970660145eb6ba0cc831c183a2401c \
-API_URI=http://proto.dev.traveltimeapp.com/api/v2/ \
-COUNTRY=NETHERLANDS \
-TRANSPORT_MODE=DRIVING \
+APP_ID={{YOUR_APP_ID}} \
+API_KEY={{YOUR_API_KEY}} \
+API_URI=https://proto.api.traveltimeapp.com/api/v2/ \
+COUNTRY=UNITED_KINGDOM \
+TRANSPORT_MODE=DRIVING_FERRY \
 TRAVEL_TIME=7200 \
 java -jar target/traveltime-benchmarks-1.0-SNAPSHOT.jar
