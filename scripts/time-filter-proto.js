1e1773e244eef62ae81bafd9a3914f9467ba6753
danielnaumau <sadfreddy@gmail.com>
2023-06-28 10:57:09 +0200

    customising metrics

--- scripts/time-filter-proto.js
+++ scripts/time-filter-proto.js
@@ -4,67 +4,47 @@ import {check, randomSeed, sleep} from 'k6';
 import {countries, destinationDeltas, generateDestinations, generateRandomCoordinate} from './common.js';
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
+const destinations = (__ENV.DESTINATIONS || '5000,10000,25000,100000')
+    .split(',')
+    .map((curDestinations) => parseInt(curDestinations))
+
+const scenarios = destinations.reduce((accumulator, currentDestinations) => {
+    accumulator[`sending_${currentDestinations}_destinations`] = {
+        executor: 'constant-vus',
+        duration: '5m',
+        env: {SCENARIO_DESTINATIONS: currentDestinations.toString()},
+
+        vus: 1,
+        startTime: '20s',
+        gracefulStop: '20s',
+    }
+    return accumulator
+}, {})
 
 const configs = {
-    scenarios: {
-        sending_5000_destinations: {
-            executor: 'constant-vus',
-            duration: '5s',
-            env: {DESTINATIONS: '5000'},
-
-            vus: 1,
-            startTime: '1s',
-            gracefulStop: '2s',
-        },
-        sending_10000_destinations: {
-            executor: 'constant-vus',
-            env: {DESTINATIONS: '10000'},
-            duration: '5s',
-            vus: 1,
-            startTime: '1s',
-            gracefulStop: '2s',
-        },
-        sending_25000_destinations: {
-            executor: 'constant-vus',
-            env: {DESTINATIONS: '25000'},
-            duration: '5s',
-            vus: 1,
-            startTime: '1s',
-            gracefulStop: '2s',
-        },
-        sending_100000_destinations: {
-            executor: 'constant-vus',
-            env: {DESTINATIONS: '100000'},
-            duration: '5s',
-            vus: 1,
-            startTime: '1s',
-            gracefulStop: '2s',
-        },
-    },
+    scenarios: scenarios,
     summaryTrendStats: ['avg', 'min', 'max', 'p(90)', 'p(95)'],
 
     thresholds: {
-        // Intentionally empty. We'll programatically define our bogus
-        // thresholds (to generate the sub-metrics) below. In your real-world
+        // Intentionally empty. I'll define bogus thresholds (to generate the sub-metrics) below.
     }
 }
 
 export let options = configs
 
+// used to create sub-metrics for each scenario
 for (let key in options.scenarios) {
     options.thresholds[`http_req_duration{scenario:${key}}`] = ['max>=0']
     options.thresholds[`http_req_receiving{scenario:${key}}`] = ['max>=0']
     options.thresholds[`http_req_sending{scenario:${key}}`] = ['max>=0']
-    options.thresholds[`http_req_connecting{scenario:${key}}`] = ['max>=0']
-
 }
 
 export default function () {
     const serviceImage = __ENV.SERVICE_IMAGE || 'unknown'
     const appId = __ENV.APP_ID
     const apiKey = __ENV.API_KEY
+    const destinationsAmount = __ENV.SCENARIO_DESTINATIONS
     const host = __ENV.HOST || 'proto.api.traveltimeapp.com'
-    const destinations = __ENV.DESTINATIONS || '100000'
     const seed = __ENV.SEED || 1234567
     const country = __ENV.COUNTRY || 'uk'
     const transportation = __ENV.TRANSPORTATION || 'driving+ferry'
@@ -73,7 +53,6 @@ export default function () {
     const travelTime = __ENV.TRAVEL_TIME || 7200
 
     const countryCoords = countries[country]
-    const destinationsAmount = parseInt(destinations)
 
     randomSeed(seed)
     const url = `${protocol}://${appId}:${apiKey}@${host}/${query}`
@@ -107,6 +86,7 @@ export default function () {
 }
 
 export function handleSummary(data) {
+    // removing default metrics
     delete data.metrics['http_req_duration']
     delete data.metrics['http_req_sending']
     delete data.metrics['http_req_receiving']
@@ -121,10 +101,9 @@ export function handleSummary(data) {
     delete data.metrics['http_req_failed']
     delete data.metrics['http_req_tls_handshaking']
 
-    data = reportPerDestination(data, 5000)
-    data = reportPerDestination(data, 10000)
-    data = reportPerDestination(data, 25000)
-    data = reportPerDestination(data, 100000)
+    data = destinations.reduce((curData, curDestinations) => {
+        return reportPerDestination(curData, curDestinations)
+    }, data)
 
     return {
         stdout: textSummary(data, { indent: ' ', enableColors: true }),
@@ -132,14 +111,12 @@ export function handleSummary(data) {
 }
 
 function reportPerDestination(data, destinations) {
-    data.metrics[`http_req_duration(${destinations} destinations)`] = data.metrics[`http_req_duration{scenario:sending_${destinations}_destinations}`]
-    delete data.metrics[`http_req_duration{scenario:sending_${destinations}_destinations}`]
     data.metrics[`http_req_sending(${destinations} destinations)`] = data.metrics[`http_req_sending{scenario:sending_${destinations}_destinations}`]
     delete data.metrics[`http_req_sending{scenario:sending_${destinations}_destinations}`]
     data.metrics[`http_req_receiving(${destinations} destinations)`] = data.metrics[`http_req_receiving{scenario:sending_${destinations}_destinations}`]
     delete data.metrics[`http_req_receiving{scenario:sending_${destinations}_destinations}`]
-    data.metrics[`http_req_connecting(${destinations} destinations)`] = data.metrics[`http_req_connecting{scenario:sending_${destinations}_destinations}`]
-    delete data.metrics[`http_req_connecting{scenario:sending_${destinations}_destinations}`]
+    data.metrics[`http_req_duration(${destinations} destinations)`] = data.metrics[`http_req_duration{scenario:sending_${destinations}_destinations}`]
+    delete data.metrics[`http_req_duration{scenario:sending_${destinations}_destinations}`]
     return data
 }
 
@@ -171,6 +148,3 @@ function generateBody(destinationsAmount, coord, transportation, travelTime) {
         }
     })
 }
-//  http_req_receiving.............: avg=7.13ms   min=32µs     med=278µs    max=27ms     p(90)=15.43ms  p(95)=16.48ms
-//  http_req_sending...............: avg=147.41µs min=63µs     med=138µs    max=525µs    p(90)=224.6µs  p(95)=245.89µs
-//
