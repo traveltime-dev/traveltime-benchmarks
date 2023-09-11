943560fa0966d70e2deef2d57766d0566f93acb2
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-09-11 14:46:35 +0300

    Fixed linting for time-map.js, time-filter.js and common.js

--- scripts/time-filter.js
+++ scripts/time-filter.js
@@ -1,128 +1,127 @@
 import {
-    textSummary
-} from 'https://jslib.k6.io/k6-summary/0.0.3/index.js';
-import http from 'k6/http';
+  textSummary
+} from 'https://jslib.k6.io/k6-summary/0.0.3/index.js'
+import http from 'k6/http'
 import {
-    check,
-    sleep
-} from 'k6';
+  check,
+  sleep
+} from 'k6'
 import {
-    generateRandomCoordinate,
-    destinations,
-    timeFilterOptions,
-    setThresholdsForScenarios,
-    countries
-} from './common.js';
+  generateRandomCoordinate,
+  destinations,
+  timeFilterOptions,
+  setThresholdsForScenarios,
+  countries
+} from './common.js'
 
-export let options = timeFilterOptions
+export const options = timeFilterOptions
 
 setThresholdsForScenarios(options)
 
-export default function() {
-    const appId = __ENV.APP_ID;
-    const apiKey = __ENV.API_KEY;
-    const host = __ENV.HOST || 'api-dev.traveltimeapp.com';
-    const countryCode = __ENV.COUNTRY || 'gb'
-    const countryCoords = countries[countryCode]
-    const url = `https://${host}/v4/time-filter`;
-    const transportation = __ENV.TRANSPORTATION || 'driving+ferry'
-    const travelTime = __ENV.TRAVEL_TIME || 1900
-    const destinationsAmount = __ENV.SCENARIO_DESTINATIONS
-    const rangeWidth = __ENV.RANGE || 0
-    const rangeSettings = {
-        enabled: rangeWidth === 0 ? false : true,
-        max_results: 3,
-        width: rangeWidth === 0 ? 1 : parseInt(rangeWidth)
-    };
-    const dateTime = new Date().toISOString()
+export default function () {
+  const appId = __ENV.APP_ID
+  const apiKey = __ENV.API_KEY
+  const host = __ENV.HOST || 'api-dev.traveltimeapp.com'
+  const countryCode = __ENV.COUNTRY || 'gb'
+  const countryCoords = countries[countryCode]
+  const url = `https://${host}/v4/time-filter`
+  const transportation = __ENV.TRANSPORTATION || 'driving+ferry'
+  const travelTime = __ENV.TRAVEL_TIME || 1900
+  const destinationsAmount = __ENV.SCENARIO_DESTINATIONS
+  const rangeWidth = __ENV.RANGE || 0
+  const rangeSettings = {
+    enabled: rangeWidth !== 0,
+    max_results: 3,
+    width: rangeWidth === 0 ? 1 : parseInt(rangeWidth)
+  }
+  const dateTime = new Date().toISOString()
 
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Application-Id': appId,
-            'X-Api-Key': apiKey
-        },
-    };
-
-    const response = http.post(url, generateBody(countryCode, travelTime,
-        transportation, destinationsAmount, rangeSettings, countryCoords, dateTime), params);
-    console.log(response.status)
-    check(response, {
-        'status is 200': (r) => r.status === 200,
-        'response body is not empty': (r) => r.body.length > 0,
-    });
-    sleep(1);
-}
-
-export function handleSummary(data) {
-    // removing default metrics
-    delete data.metrics['http_req_duration']
-    delete data.metrics['http_req_sending']
-    delete data.metrics['http_req_receiving']
-    delete data.metrics['http_req_blocked']
-    delete data.metrics[`http_req_duration{expected_response:true}`]
-    delete data.metrics['http_req_waiting']
-    delete data.metrics['http_reqs']
-    delete data.metrics['iteration_duration']
-    delete data.metrics['iterations']
-    delete data.metrics['vus']
-    delete data.metrics['http_req_connecting']
-    delete data.metrics['http_req_failed']
-    delete data.metrics['http_req_tls_handshaking']
-
-    data = destinations.reduce((curData, curDestinations) => {
-        return reportPerDestination(curData, curDestinations)
-    }, data)
-
-    return {
-        stdout: textSummary(data, {
-            indent: ' ',
-            enableColors: true
-        }),
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Application-Id': appId,
+      'X-Api-Key': apiKey
     }
+  }
+
+  const response = http.post(url, generateBody(countryCode, travelTime,
+    transportation, destinationsAmount, rangeSettings, countryCoords, dateTime), params)
+  check(response, {
+    'status is 200': (r) => r.status === 200,
+    'response body is not empty': (r) => r.body.length > 0
+  })
+  sleep(1)
 }
 
-function reportPerDestination(data, destinations) {
-    data.metrics[`http_req_sending(${destinations} destinations)`] =
+export function handleSummary (data) {
+  // removing default metrics
+  delete data.metrics.http_req_duration
+  delete data.metrics.http_req_sending
+  delete data.metrics.http_req_receiving
+  delete data.metrics.http_req_blocked
+  delete data.metrics['http_req_duration{expected_response:true}']
+  delete data.metrics.http_req_waiting
+  delete data.metrics.http_reqs
+  delete data.metrics.iteration_duration
+  delete data.metrics.iterations
+  delete data.metrics.vus
+  delete data.metrics.http_req_connecting
+  delete data.metrics.http_req_failed
+  delete data.metrics.http_req_tls_handshaking
+
+  data = destinations.reduce((curData, curDestinations) => {
+    return reportPerDestination(curData, curDestinations)
+  }, data)
+
+  return {
+    stdout: textSummary(data, {
+      indent: ' ',
+      enableColors: true
+    })
+  }
+}
+
+function reportPerDestination (data, destinations) {
+  data.metrics[`http_req_sending(${destinations} destinations)`] =
         data.metrics[`http_req_sending{scenario:sending_${destinations}_destinations}`]
-    delete data.metrics[`http_req_sending{scenario:sending_${destinations}_destinations}`]
-    data.metrics[`http_req_receiving(${destinations} destinations)`] =
+  delete data.metrics[`http_req_sending{scenario:sending_${destinations}_destinations}`]
+  data.metrics[`http_req_receiving(${destinations} destinations)`] =
         data.metrics[`http_req_receiving{scenario:sending_${destinations}_destinations}`]
-    delete data.metrics[`http_req_receiving{scenario:sending_${destinations}_destinations}`]
-    data.metrics[`http_req_duration(${destinations} destinations)`] =
+  delete data.metrics[`http_req_receiving{scenario:sending_${destinations}_destinations}`]
+  data.metrics[`http_req_duration(${destinations} destinations)`] =
         data.metrics[`http_req_duration{scenario:sending_${destinations}_destinations}`]
-    delete data.metrics[`http_req_duration{scenario:sending_${destinations}_destinations}`]
-    return data
+  delete data.metrics[`http_req_duration{scenario:sending_${destinations}_destinations}`]
+  return data
 }
 
-function generateBody(countryCode, travelTime, transportation, destinationsAmount, rangeSettings,
-    countryCoords, dateTime) {
-    const coordinates = countryCoords
+function generateBody (countryCode, travelTime, transportation, destinationsAmount, rangeSettings,
+  countryCoords, dateTime) {
+  const coordinates = countryCoords
 
-    const destinations = Array.from({
-        length: destinationsAmount
-    }, (_, i) => ({
-        id: `destination${i + 1}`,
-        coords: generateRandomCoordinate(coordinates.lat, coordinates.lng, 0.005),
-    }));
+  const destinations = Array.from({
+    length: destinationsAmount
+  }, (_, i) => ({
+    id: `destination${i + 1}`,
+    coords: generateRandomCoordinate(coordinates.lat, coordinates.lng, 0.005)
+  }))
 
-    const departureSearches = [{
-        id: 'Time filter benchmark',
-        departure_location_id: 'destination1',
-        arrival_location_ids: destinations.map(destination => destination.id),
-        departure_time: dateTime,
-        travel_time: travelTime,
-        properties: [
-            'travel_time'
-        ],
-        transportation: {
-            type: transportation
-        },
-        range: rangeSettings
-    }];
+  const departureSearches = [{
+    id: 'Time filter benchmark',
+    departure_location_id: 'destination1',
+    arrival_location_ids: destinations.map(destination => destination.id),
+    departure_time: dateTime,
+    travel_time: travelTime,
+    properties: [
+      'travel_time'
+    ],
+    transportation: {
+      type: transportation
+    },
+    range: rangeSettings
+  }]
 
-    return JSON.stringify({
-        locations: destinations,
-        departure_searches: departureSearches
-    });
+  return JSON.stringify({
+    locations: destinations,
+    departure_searches: departureSearches
+  })
 }
--- scripts/time-map.js
+++ scripts/time-map.js
@@ -1,80 +1,76 @@
 import {
-    textSummary
-} from 'https://jslib.k6.io/k6-summary/0.0.3/index.js';
-import http from 'k6/http';
+  textSummary
+} from 'https://jslib.k6.io/k6-summary/0.0.3/index.js'
+import http from 'k6/http'
 import {
-    check,
-    sleep
-} from 'k6';
+  check,
+  sleep
+} from 'k6'
 import {
-    generateRandomCoordinate,
-    countries,
-    mapEndpointOptions
-} from './common.js';
+  generateRandomCoordinate,
+  countries,
+  mapEndpointOptions
+} from './common.js'
 
+export const options = mapEndpointOptions
 
-export let options = mapEndpointOptions
-
-const country = __ENV.COUNTRY || 'gb'
-
-export default function() {
-    const appId = __ENV.APP_ID
-    const apiKey = __ENV.API_KEY
-    const host = __ENV.HOST || 'api-dev.traveltimeapp.com'
-    const countryCode = __ENV.COUNTRY || 'gb'
-    const countryCoords = countries[countryCode]
-    const url = `https://${host}/v4/time-map`;
-    const transportation = __ENV.TRANSPORTATION || 'driving+ferry'
-    const travelTime = __ENV.TRAVEL_TIME || 7200
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Application-Id': appId,
-            'X-Api-Key': apiKey
-        },
-    };
-    const dateTime = new Date().toISOString()
-
-    const response = http.post(url, generateBody(countryCode, travelTime, transportation, countryCoords, dateTime), params);
-    console.log(response.status)
-    check(response, {
-        'status is 200': (r) => r.status === 200,
-        'response body is not empty': (r) => r.body.length > 0,
-    });
-    sleep(1);
-}
-
-export function handleSummary(data) {
-    delete data.metrics['http_req_blocked']
-    delete data.metrics[`http_req_duration{expected_response:true}`]
-    delete data.metrics['http_req_waiting']
-    delete data.metrics['http_reqs']
-    delete data.metrics['iteration_duration']
-    delete data.metrics['iterations']
-    delete data.metrics['vus']
-    delete data.metrics['http_req_connecting']
-    delete data.metrics['http_req_failed']
-    delete data.metrics['http_req_tls_handshaking']
-
-    return {
-        stdout: textSummary(data, {
-            indent: ' ',
-            enableColors: true
-        }),
+export default function () {
+  const appId = __ENV.APP_ID
+  const apiKey = __ENV.API_KEY
+  const host = __ENV.HOST || 'api-dev.traveltimeapp.com'
+  const countryCode = __ENV.COUNTRY || 'gb'
+  const countryCoords = countries[countryCode]
+  const url = `https://${host}/v4/time-map`
+  const transportation = __ENV.TRANSPORTATION || 'driving+ferry'
+  const travelTime = __ENV.TRAVEL_TIME || 7200
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Application-Id': appId,
+      'X-Api-Key': apiKey
     }
+  }
+  const dateTime = new Date().toISOString()
+
+  const response = http.post(url, generateBody(countryCode, travelTime, transportation, countryCoords, dateTime), params)
+  check(response, {
+    'status is 200': (r) => r.status === 200,
+    'response body is not empty': (r) => r.body.length > 0
+  })
+  sleep(1)
 }
 
-function generateBody(countryCode, travelTime, transportation, countryCoords, dateTime) {
-    const coordinates = countryCoords
-    return JSON.stringify({
-        departure_searches: [{
-            id: 'Time map benchmark',
-            coords: generateRandomCoordinate(coordinates.lat, coordinates.lng, 0.005),
-            departure_time: dateTime,
-            travel_time: travelTime,
-            transportation: {
-                type: transportation
-            }
-        }]
-    });
+export function handleSummary (data) {
+  delete data.metrics.http_req_blocked
+  delete data.metrics['http_req_duration{expected_response:true}']
+  delete data.metrics.http_req_waiting
+  delete data.metrics.http_reqs
+  delete data.metrics.iteration_duration
+  delete data.metrics.iterations
+  delete data.metrics.vus
+  delete data.metrics.http_req_connecting
+  delete data.metrics.http_req_failed
+  delete data.metrics.http_req_tls_handshaking
+
+  return {
+    stdout: textSummary(data, {
+      indent: ' ',
+      enableColors: true
+    })
+  }
+}
+
+function generateBody (countryCode, travelTime, transportation, countryCoords, dateTime) {
+  const coordinates = countryCoords
+  return JSON.stringify({
+    departure_searches: [{
+      id: 'Time map benchmark',
+      coords: generateRandomCoordinate(coordinates.lat, coordinates.lng, 0.005),
+      departure_time: dateTime,
+      travel_time: travelTime,
+      transportation: {
+        type: transportation
+      }
+    }]
+  })
 }
