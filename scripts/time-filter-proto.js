9a9798abeff7b539f2310eb4c0aa30e5edfb8dfb
arnasbr <arnas.bradauskas@traveltime.com>
2024-04-05 15:57:03 +0300

    Time filter proto

--- scripts/time-filter-proto.js
+++ scripts/time-filter-proto.js
@@ -15,7 +15,9 @@ import {
   deleteTimeFilterMetrics,
   summaryTrendStats,
   reportPerDestination,
-  getProtoCountryCoordinates
+  getProtoCountryCoordinates,
+  generateRequestBodies,
+  randomIndex
 } from './common.js'
 import {
   textSummary
@@ -30,15 +32,15 @@ export const options = {
   }
 }
 
-setThresholdsForScenarios(options)
-randomSeed(__ENV.SEED || 1234567)
+export function setup () {
+  setThresholdsForScenarios(options)
+  randomSeed(__ENV.SEED || 1234567)
 
-export default function () {
   const serviceImage = __ENV.SERVICE_IMAGE || 'unknown'
   const mapDate = __ENV.MAP_DATE || 'unknown'
   const appId = __ENV.APP_ID
   const apiKey = __ENV.API_KEY
-  const destinationsAmount = __ENV.SCENARIO_DESTINATIONS
+  const destinationsAmount = __ENV.SCENARIO_DESTINATIONS || 1
   const host = __ENV.HOST || 'proto.api.traveltimeapp.com'
   const transportation = __ENV.TRANSPORTATION || 'driving+ferry'
   const protocol = __ENV.PROTOCOL || 'https'
@@ -48,28 +50,36 @@ export default function () {
   const country = envCountry || 'uk'
   const query = __ENV.QUERY || `api/v2/${countryCode(country)}/time-filter/fast/${transportation}`
   const isManyToOne = __ENV.MANY_TO_ONE !== undefined
+  const uniqueRequests = parseInt(__ENV.UNIQUE_REQUESTS || 1)
+  const params = {
+    headers: {
+      'Content-Type': 'application/octet-stream'
+    },
+    tags: {
+      destinations: destinationsAmount,
+      serviceImage,
+      mapDate
+    }
+  }
 
   const url = `${protocol}://${appId}:${apiKey}@${host}/${query}`
 
-  const requestBody = protobuf
-    .load('proto/TimeFilterFastRequest.proto', 'TimeFilterFastRequest')
-    .encode(generateBody(destinationsAmount, countryCoords, transportation, travelTime, isManyToOne))
+  const requestBodies = generateRequestBodies(uniqueRequests, generateBody, destinationsAmount, countryCoords, transportation, travelTime, isManyToOne)
 
-  const response = http.post(
-    url,
-    requestBody, {
-      headers: {
-        'Content-Type': 'application/octet-stream'
-      },
-      tags: {
-        destinations: destinationsAmount,
-        serviceImage,
-        mapDate
-      }
-    }
-  )
+  const encodedRequestBodies = encodeBodies(requestBodies)
+
+  return { url, encodedRequestBodies, params }
+}
+
+export default function (data) {
+  const index = randomIndex(data.encodedRequestBodies.length)
+  const response = http.post(data.url, data.encodedRequestBodies[index], data.params)
 
   const decodedResponse = protobuf.load('proto/TimeFilterFastResponse.proto', 'TimeFilterFastResponse').decode(response.body)
+
+  console.log(response.status)
+  console.log(response.body)
+  console.log(decodedResponse)
 
   check(response, {
     'status is 200': (r) => r.status === 200
@@ -115,6 +125,13 @@ function transportationType (transportation) {
 
 function countryCode (country) {
   if (country.startsWith('us_')) { return 'us' } else { return country }
+}
+
+function encodeBodies (bodies) {
+  const proto = protobuf.load('proto/TimeFilterFastRequest.proto', 'TimeFilterFastRequest')
+  const encodedBodies = bodies.map(body => proto.encode(body))
+  console.log(encodedBodies[0])
+  return encodedBodies
 }
 
 function generateBody (destinationsAmount, coord, transportation, travelTime, isManyToOne) {
